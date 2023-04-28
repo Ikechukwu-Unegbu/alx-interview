21969792548165c84f8da119a02596ebbde94a33
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-04-28 11:42:47 +0100

    progress

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,52 +1,62 @@
 #!/usr/bin/python3
+'''N Queens Challenge'''
+
 import sys
 
-if __name__ == "__main__":
-    # Check number of arguments
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
-    # Parse N from command line argument
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
+        print('N must be a number')
         sys.exit(1)
 
-    # Check if N is valid
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         sys.exit(1)
 
-    # Initialize the chess board
-    board = [[0 for j in range(n)] for i in range(n)]
-
-    # Initialize solution list
     solutions = []
+    queens = [] # format: [row, column]
+    row, col = 0, 0
 
-    # Iterate over rows
-    for i in range(n):
-        # Iterate over columns
-        for j in range(n):
-            # Place queen if it's safe
-            if all(board[k][j] == 0 for k in range(i)) \
-               and all(board[i][k] == 0 for k in range(j)) \
-               and all(board[i-k][j-k] == 0 for k in range(min(i,j)+1)) \
-               and all(board[i-k][j+k] == 0 for k in range(min(i,n-j))):
-                board[i][j] = 1
-
-                # Check if it's a solution
-                if i == n-1:
-                    solutions.append([[i,j] for i,j in enumerate(board[-1]) if j == 1])
-                else:
+    while row < n:
+        is_safe = True
+        while col < n:
+            for q in queens:
+                q_row, q_col = q[0], q[1]
+                if col == q_col or row+col == q_row+q_col or row-col == q_row-q_col:
+                    is_safe = False
                     break
-            else:
-                # Remove previous queens and backtrack
-                for k in range(i+1):
-                    board[k][j-1] = 0
-                if j == 0:
-                    print(solutions)
-                    sys.exit(0)
-                j -= 2
+            
+            if is_safe:
+                queens.append([row, col])
+                if row == n-1:
+                    solutions.append(queens[:])
+                    queens.pop()
+                    if col == n-1:
+                        queens.pop()
+                        if len(queens) == 0:
+                            break
+                        col = queens.pop()[1] + 1
+                        row -= 1
+                    else:
+                        col += 1
+                else:
+                    col = 0
+                    row += 1
                 break
+
+            if col == n-1:
+                queens.pop()
+                if len(queens) == 0:
+                    break
+                col = queens.pop()[1] + 1
+                row -= 1
+            else:
+                col += 1
+        
+    for sol in solutions:
+        print(sol)
